d8767ffe92de3d9629559ce738f7e1e1c01f9882
joeysullivann <joeysull03@gmail.com>
2017-05-23 11:19:35 -0700

    kjhbjhg

---
+++ Carsdotcom/src/sample/Carsdotcom.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by sullivanj3341 on 5/23/2017.
+ */
+public class Carsdotcom {
+    private JButton Login;
+    private JPanel panelMain;
+
+    public Carsdotcom() {
+        Login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Login");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Carsdotcom");
+        frame.setContentPane(new Carsdotcom().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
+
+
